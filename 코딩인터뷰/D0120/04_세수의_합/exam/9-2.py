454c40b20856f2c919d6832c9d0ab392e7fb72f9
seohae <seohae0406@gmail.com>
2021-01-21 19:20:47 +0900

    01/19 ~ 01/20 exam(답 코드) 코드 이해하기

--- 코딩인터뷰/D0120/04_세수의_합/exam/9-2.py
+++ 코딩인터뷰/D0120/04_세수의_합/exam/9-2.py
@@ -29,12 +29,26 @@ class Solution:
                     # `sum = 0`인 경우이므로 정답 및 스킵 처리
                     results.append([nums[i], nums[left], nums[right]])
 
+                    # 아래 로직은 중복 제거를 위한 로직이다.
+
+                    # left 가 right 보다 작을때
+                    # nums[left] 가 다음 원소와 같을때 다음 원소 차례로 left 를 바꿔준다.
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
+
+                    # left 가 right 보다 작을때
+                    # nums[right] 가 right 의 다음 원소(왼쪽)와 같을때 다음(왼쪽) 원소 차례로 right 를 바꿔준다.
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
 
+                    # 다음 for 문의 left, right 를 지정해주기 위함이다.
                     left += 1
                     right -= 1
 
         return results
+
+
+f = Solution()
+
+# [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+print(Solution.threeSum(f, [-1, 0, 1, 2, -1, -4]))
