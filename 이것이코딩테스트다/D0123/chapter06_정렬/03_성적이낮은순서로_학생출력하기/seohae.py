68083ddd5b739ec6a2571983d818ecef7edb4f9e
seohae <seohae0406@gmail.com>
2021-01-23 18:03:38 +0900

    D0123

---
+++ 이것이코딩테스트다/D0123/chapter06_정렬/03_성적이낮은순서로_학생출력하기/seohae.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+array = []
+
+for i in range(n):
+    input_data = input().split()
+
+    # 이름은 문자열
+    # 점수는 정수형으로 변환하여 저장
+    array.append((input_data[0], int(input_data[1])))
+
+# 점수 오름차순
+array = sorted(array, key=lambda student: student[1])
+print(array)
+
+for student in array:
+    print(student[0], end=' ')
+
