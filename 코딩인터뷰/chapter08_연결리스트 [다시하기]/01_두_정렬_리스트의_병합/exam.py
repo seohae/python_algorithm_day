b2c64c2a72a76b7ffcf0782b0cce3e7a87d87f8e
seohae <seohae0406@gmail.com>
2021-02-07 12:25:51 +0900

    D0207 연결리스트(2)

--- 코딩인터뷰/chapter08_연결리스트 [다시하기]/01_두_정렬_리스트의_병합/exam.py
+++ 코딩인터뷰/chapter08_연결리스트 [다시하기]/01_두_정렬_리스트의_병합/exam.py
@@ -1,3 +1,18 @@
-# 정렬되어있는 두 연결 리스트를 합쳐라
-# 입력 1->2->4, 1->3->4
-# 출력 1->1->2->3->4->4
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+        if l1:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+
+
+
+# 정리 : https://devfunny.tistory.com/416
